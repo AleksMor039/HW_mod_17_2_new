ec01b42e7552e866c803a511354dd4998943ca0a
AleksMor039 <aleks.mor045@gmail.com>
2024-12-22 17:22:29 +0200

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,22 +1,29 @@
-from fastapi import APIRouter
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
+from sqlalchemy.orm import relationship  # взаимосвязь
+from app.models import *
 
-router = APIRouter(prefix="/user", tags=["user"])
+'''
+В user.py создать модель User, 
+наслед. от ранее написанного Base со следующими атрибутами:
+'''
 
-@router.get("/")
-async def all_users():
-    pass
 
-@router.get("/user_id")
-async def user_by_id():
-    pass
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {"extend_existing": True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
 
-@router.post("/create")
-async def create_user():
-    pass
+    # relationship - показ.взаимосвязь (к кому взаимосвязь) и (куда возвращаться с ней)
+    # объект связи с табл. Task
+    tasks = relationship("Task", back_populates="user")
 
-@router.put("/update")
-async def update_user():
-    pass
-@router.delete("/delete")
-async def delete_user():
-    pass
+
+from sqlalchemy.schema import CreateTable
+
+print(CreateTable(User.__table__))
